0cbad3d64df5451f712aa5eb19c6945607f0be12
piccolomo <56689657+piccolomo@users.noreply.github.com>
2022-02-05 14:44:32 +0000

    4.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author = "Savino Piccolomo",
     author_email = "piccolomo@gmail.com",
     name = 'plotext',
-    version='4.1.6',
+    version='4.1.7',
     description = 'plotext plots directly on terminal',
     long_description = README,
     long_description_content_type = "text/markdown",  
