8ff5602563d3dd1e9cd61b5e92cacb1370b20e9d
piccolomo <56689657+piccolomo@users.noreply.github.com>
2023-08-12 17:38:45 +0100

    first update to box docstring
    It still needs some work

--- plotext/_doc.py
+++ plotext/_doc.py
@@ -232,25 +232,6 @@ past("xside", "scatter")
 past("yside", "scatter")
 past("label", "scatter")
 
-"""
-box(self, *args, xside = None, yside = None, 
-    orientation = None, colors = None, label = None,
-    fill=None, width = None, minimum = None, 
-    offset = None, reset_ticks = None, hint = None):
-"""
-method("box")
-doc("It creates a box plot depend on data provieded")
-par("args", "grouped data or specific boundary values.")
-par("fill", "if True, the bars will be filled with the the given colored markers, otherwise only its borders would be printed.", t.bool, True)
-par("width", "the relative width of the bars, expressed as a float ranging from 0 to 1.", 'number', 4 / 5)
-par("minimum", "the minimum value of all the bars; this value could be easily tweaked, particularly when a logarithmic scale is chosen along the bars height.", 'number', 0)
-par("reset_ticks", "if True, the center coordinates at the base of each bar will be calculated (starting from 1) and printed automatically; otherwise the numerical ticks, along the base of the bars, will be calculated as for any other plot.", t.bool, True)
-par("offset", "offset position of the label.")
-past("orientation", "bar")
-past("xside", "scatter")
-past("yside", "scatter")
-past("label", "scatter")
-
 
 method("bar") 
 doc("It creates a bar plot using the x and y data provided.")
@@ -342,6 +323,34 @@ past("yside", "scatter")
 past("label", "scatter")
 
 
+method("box")
+doc("It creates a box plot.")
+par("args", "the grouped data to plot or its specific boundaries. need a type and a default value???")
+par("hint", "??? need a description a type and a default value??")
+par("colors", "??? need a description a type and a default value??")
+past("fill", "bar")
+past("width", "bar")
+past("orientation", "bar")
+past("minimum", "bar")
+past("reset_ticks", "bar")
+past("xside", "scatter")
+past("yside", "scatter")
+past("label", "scatter")
+
+
+method("bar") 
+doc("It creates a bar plot using the x and y data provided.")
+par("args", "The coordinates x and y (or just y), of the bars; string labels or dates are accepted (but only as x values).", t.xy)
+par("marker", "the marker used to identify the bars, which could be a single character or the available marker codes, accessible with the markers() method.", t.str, 'sd')
+par("color", "the color code of the bars, accessible with the colors() method.", t.color, 'blue+')
+par("fill", "if True, the bars will be filled with the the given colored markers, otherwise only its borders would be printed.", t.bool, True)
+par("width", "the relative width of the bars, expressed as a float ranging from 0 to 1.", 'number', 4 / 5)
+past("orientation", "candlestick")
+par("minimum", "the minimum value of all the bars; this value could be easily tweaked, particularly when a logarithmic scale is chosen along the bars height.", 'number', 0)
+par("reset_ticks", "if True, the center coordinates at the base of each bar will be calculated (starting from 1) and printed automatically; otherwise the numerical ticks, along the base of the bars, will be calculated as for any other plot.", t.bool, True)
+past("xside", "scatter")
+past("yside", "scatter")
+past("label", "scatter")
 
 method("error") 
 doc("It build a scatter plot with error bars.\n\nNote that no marker parameter is needed, as the markers are automatically set to straight lines.")
