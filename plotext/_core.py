f88200503246daf5a39c1a5b09734798821eb134
piccolomo <56689657+piccolomo@users.noreply.github.com>
2023-08-12 17:31:09 +0100

    removed offset parameter of box plot in file _core.py
    the offset parameter in the draw_bar method is used only internally and not by the user, so unless I misunderstood, the same should happen with box plot.

--- plotext/_core.py
+++ plotext/_core.py
@@ -188,10 +188,6 @@ def candlestick(dates, data, xside = None, yside = None, orientation = None, col
     _figure._active.candlestick(dates, data, xside = xside, yside = yside, orientation = orientation, colors = colors, label = label)
     _figure.show() if _figure._interactive else None
 
-def box(*args, xside = None, yside = None, orientation = None, colors = None, label = None, fill = None, width = None, minimum = None, offset = None, reset_ticks = None, hint = None):
-    _figure._active.box(*args, xside = xside, yside = yside, orientation = orientation, colors = colors, label = label, fill=fill, width = width, minimum = minimum,  offset = offset, reset_ticks = reset_ticks, hint = hint)
-    _figure.show() if _figure._interactive else None
-
 def bar(*args, marker = None, color = None, fill = None, width = None, orientation = None, minimum = None, reset_ticks = None, xside = None, yside = None, label = None):
     _figure._active.bar(*args, xside = xside, yside = yside, marker = marker, color = color, fill = fill, width = width, orientation = orientation, label = label, minimum = minimum, reset_ticks = reset_ticks)
     _figure.show() if _figure._interactive else None
@@ -222,6 +218,10 @@ def hist(data, bins = None, marker = None, color = None, fill = None, norm = Non
 
 def candlestick(dates, data, colors = None, orientation = None, xside = None, yside = None, label = None):
     _figure._active.candlestick(dates, data, xside = xside, yside = yside, orientation = orientation, colors = colors, label = label)
+    _figure.show() if _figure._interactive else None
+
+def box(*args, xside = None, yside = None, orientation = None, colors = None, label = None, fill = None, width = None, minimum = None reset_ticks = None, hint = None):
+    _figure._active.box(*args, xside = xside, yside = yside, orientation = orientation, colors = colors, label = label, fill=fill, width = width, minimum = minimum, reset_ticks = reset_ticks, hint = hint)
     _figure.show() if _figure._interactive else None
 
 ##############################################
