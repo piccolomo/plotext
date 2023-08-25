ff6c9fabba4b433a9efd92498d10d617d7589ee3
Savino Piccolomo <piccolomo@gmail.com>
2023-08-25 19:29:25 +0100

    version 5.3.1: box plot available

--- plotext/__init__.py
+++ plotext/__init__.py
@@ -1,7 +1,7 @@
 """\nplotext plots directly on terminal"""
     
 __name__ = "plotext"
-__version__ = "5.3.0"
+__version__ = "5.3.1"
 
 from ._core import *
 #from .plotext_cli import build_parser # not sure why this line was here
