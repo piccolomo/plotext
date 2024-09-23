4d19108b93e34a60ba789681756450ae126a76ed
piccolomo <56689657+piccolomo@users.noreply.github.com>
2024-09-23 16:51:26 +0100

    Version 5.3.2 __init__.py update

--- plotext/__init__.py
+++ plotext/__init__.py
@@ -1,7 +1,7 @@
 """\nplotext plots directly on terminal"""
     
 __name__ = "plotext"
-__version__ = "5.3.1"
+__version__ = "5.3.2"
 
 from ._core import *
 #from .plotext_cli import build_parser # not sure why this line was here
